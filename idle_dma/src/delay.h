dcbac8229d27d6d5571b2ee01d2d8f978c906ef9
wolf <137526291@qq.com>
2025-10-08 10:39:28 +0800

    INIT. u1 u2 rx idle dma ok

---
+++ idle_dma/src/delay.h
@@ -0,0 +1,7 @@
+
+#include <stdint.h>
+/* delay function */
+void delay_init(void);
+void delay_us(uint32_t nus);
+void delay_ms(uint16_t nms);
+void delay_sec(uint16_t sec);
---
+++ idle_dma/src/main.c
@@ -0,0 +1,333 @@
+/**
+  **************************************************************************
+  * @file     main.c
+  * @brief    main program
+  **************************************************************************
+  *                       Copyright notice & Disclaimer
+  *
+  * The software Board Support Package (BSP) that is made available to
+  * download from Artery official website is the copyrighted work of Artery.
+  * Artery authorizes customers to use, copy, and distribute the BSP
+  * software and its related documentation for the purpose of design and
+  * development in conjunction with Artery microcontrollers. Use of the
+  * software is governed by this copyright notice and the following disclaimer.
+  *
+  * THIS SOFTWARE IS PROVIDED ON "AS IS" BASIS WITHOUT WARRANTIES,
+  * GUARANTEES OR REPRESENTATIONS OF ANY KIND. ARTERY EXPRESSLY DISCLAIMS,
+  * TO THE FULLEST EXTENT PERMITTED BY LAW, ALL EXPRESS, IMPLIED OR
+  * STATUTORY OR OTHER WARRANTIES, GUARANTEES OR REPRESENTATIONS,
+  * INCLUDING BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+  * FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT.
+  *
+  **************************************************************************
+  */
+
+#include "at32f421_board.h"
+#include "at32f421_clock.h"
+#include "log.h"
+
+#define COUNTOF(a)                       (sizeof(a) / sizeof(*(a)))
+#define USART2_TX_BUFFER_SIZE            (COUNTOF(usart2_tx_buffer) - 1)
+#define USART1_TX_BUFFER_SIZE            (COUNTOF(usart1_tx_buffer) - 1)
+#define USART1_RX_BUFFER_SIZE            64
+#define USART2_RX_BUFFER_SIZE            1024 //master u2 pa2 pa15 connect to BLE
+
+uint8_t usart2_tx_buffer[] = "usart transfer by dma interrupt: usart2 -> usart1 using dma";
+uint8_t usart1_tx_buffer[] = "usart transfer by dma interrupt: usart1 -> usart2 using dma";
+
+uint8_t usart1_rx_buffer[USART1_RX_BUFFER_SIZE];
+uint8_t usart2_rx_buffer[USART2_RX_BUFFER_SIZE];
+
+void u1rx_idle_cb_func(uint8_t* buf, uint16_t len);
+void u2rx_idle_cb_func(uint8_t* buf, uint16_t len);
+typedef void (*cb_func)(uint8_t* buf, uint16_t len);
+
+void u1rx_idle_cb_func(uint8_t* buf, uint16_t len)
+{
+  printf("u1idma_cb: %s\r\n", buf);
+}
+
+void u2rx_idle_cb_func(uint8_t* buf, uint16_t len)
+{
+  printf("u2idma_cb: %3d = %s\r\n", len, buf);
+}
+
+cb_func rxidle_cb_u1 = u1rx_idle_cb_func;
+cb_func rxidle_cb_u2 = u2rx_idle_cb_func;
+
+/**
+  * @brief  initialize uart1 in pb6 pb7
+  * @param  baudrate: uart baudrate
+  * @retval none
+  */
+void uart1_init(uint32_t baudrate)
+{
+  gpio_init_type gpio_init_struct;
+  dma_init_type dma_init_struct;
+
+#if defined (__GNUC__) && !defined (__clang__)
+  setvbuf(stdout, NULL, _IONBF, 0);
+#endif
+
+  /* enable the uart and gpio clock */
+  crm_periph_clock_enable(CRM_USART1_PERIPH_CLOCK, TRUE);
+  crm_periph_clock_enable(CRM_GPIOB_PERIPH_CLOCK, TRUE);
+  crm_periph_clock_enable(CRM_DMA1_PERIPH_CLOCK, TRUE);
+
+  gpio_default_para_init(&gpio_init_struct);
+
+  /* configure the uart tx pin */
+  gpio_init_struct.gpio_drive_strength = GPIO_DRIVE_STRENGTH_STRONGER;
+  gpio_init_struct.gpio_out_type  = GPIO_OUTPUT_PUSH_PULL;
+  gpio_init_struct.gpio_mode = GPIO_MODE_MUX;
+  gpio_init_struct.gpio_pins = GPIO_PINS_6 | GPIO_PINS_7;
+  gpio_init_struct.gpio_pull = GPIO_PULL_NONE;
+  gpio_init(GPIOB, &gpio_init_struct);
+
+  gpio_pin_mux_config(GPIOB, GPIO_PINS_SOURCE6, GPIO_MUX_0);
+  gpio_pin_mux_config(GPIOB, GPIO_PINS_SOURCE7, GPIO_MUX_0);
+
+  /* dma1 channel2 for usart1 tx configuration */
+  dma_reset(DMA1_CHANNEL2);
+  dma_default_para_init(&dma_init_struct);
+  dma_init_struct.buffer_size = USART1_TX_BUFFER_SIZE;
+  dma_init_struct.direction = DMA_DIR_MEMORY_TO_PERIPHERAL;
+  dma_init_struct.memory_base_addr = (uint32_t)usart1_tx_buffer;
+  dma_init_struct.memory_data_width = DMA_MEMORY_DATA_WIDTH_BYTE;
+  dma_init_struct.memory_inc_enable = TRUE;
+  dma_init_struct.peripheral_base_addr = (uint32_t)&USART1->dt;
+  dma_init_struct.peripheral_data_width = DMA_PERIPHERAL_DATA_WIDTH_BYTE;
+  dma_init_struct.peripheral_inc_enable = FALSE;
+  dma_init_struct.priority = DMA_PRIORITY_MEDIUM;
+  dma_init_struct.loop_mode_enable = FALSE;
+  dma_init(DMA1_CHANNEL2, &dma_init_struct);
+
+  /* dma1 channel3 for usart1 rx configuration */
+  dma_reset(DMA1_CHANNEL3);
+  dma_default_para_init(&dma_init_struct);
+  dma_init_struct.buffer_size = USART1_RX_BUFFER_SIZE; //rxcnt should never exceed this value
+  dma_init_struct.direction = DMA_DIR_PERIPHERAL_TO_MEMORY;
+  dma_init_struct.memory_base_addr = (uint32_t)usart1_rx_buffer;
+  dma_init_struct.memory_data_width = DMA_MEMORY_DATA_WIDTH_BYTE;
+  dma_init_struct.memory_inc_enable = TRUE;
+  dma_init_struct.peripheral_base_addr = (uint32_t)&USART1->dt;
+  dma_init_struct.peripheral_data_width = DMA_PERIPHERAL_DATA_WIDTH_BYTE;
+  dma_init_struct.peripheral_inc_enable = FALSE;
+  dma_init_struct.priority = DMA_PRIORITY_MEDIUM;
+  dma_init_struct.loop_mode_enable = FALSE;
+  dma_init(DMA1_CHANNEL3, &dma_init_struct);
+
+  /* configure uart param */
+  usart_init(USART1, baudrate, USART_DATA_8BITS, USART_STOP_1_BIT);
+  usart_transmitter_enable(USART1, TRUE);
+  usart_receiver_enable(USART1, TRUE);
+  usart_interrupt_enable(USART1, USART_IDLE_INT, TRUE);
+  
+
+  usart_dma_transmitter_enable(USART1, TRUE);
+  usart_dma_receiver_enable(USART1, TRUE);
+
+  dma_interrupt_enable(DMA1_CHANNEL3, DMA1_FDT3_FLAG, TRUE);//in dma idle mode, FDT should never happen.
+  nvic_irq_enable(DMA1_Channel3_2_IRQn, 3, 0);
+  nvic_irq_enable(USART1_IRQn, 1, 0);
+
+  dma_channel_enable(DMA1_CHANNEL3, TRUE); /* usart1 rx begin dma receiving */
+  usart_enable(USART1, TRUE);
+}
+
+/**
+  * @brief  initialize uart1 in pb6 pb7
+  * @param  baudrate: uart baudrate
+  * @retval none
+  */
+void uart2_init(uint32_t baudrate)
+{
+  gpio_init_type gpio_init_struct;
+  dma_init_type dma_init_struct;
+
+#if defined (__GNUC__) && !defined (__clang__)
+  setvbuf(stdout, NULL, _IONBF, 0);
+#endif
+
+  /* enable the uart and gpio clock */
+  crm_periph_clock_enable(CRM_USART2_PERIPH_CLOCK, TRUE);
+  crm_periph_clock_enable(CRM_GPIOA_PERIPH_CLOCK, TRUE);
+  crm_periph_clock_enable(CRM_DMA1_PERIPH_CLOCK, TRUE);
+
+  gpio_default_para_init(&gpio_init_struct);
+
+  /* configure the uart tx pin */
+  gpio_init_struct.gpio_drive_strength = GPIO_DRIVE_STRENGTH_MODERATE;
+  gpio_init_struct.gpio_out_type  = GPIO_OUTPUT_PUSH_PULL;
+  gpio_init_struct.gpio_mode = GPIO_MODE_MUX;
+  gpio_init_struct.gpio_pins = GPIO_PINS_2 | GPIO_PINS_15;
+  gpio_init_struct.gpio_pull = GPIO_PULL_NONE;
+  gpio_init(GPIOA, &gpio_init_struct);
+
+  gpio_pin_mux_config(GPIOA, GPIO_PINS_SOURCE2, GPIO_MUX_1);
+  gpio_pin_mux_config(GPIOA, GPIO_PINS_SOURCE15, GPIO_MUX_1);
+
+
+  // /* dma1 channel4 for usart2 tx configuration */
+  dma_reset(DMA1_CHANNEL4);
+  dma_default_para_init(&dma_init_struct);
+  dma_init_struct.buffer_size = USART2_TX_BUFFER_SIZE;
+  dma_init_struct.direction = DMA_DIR_MEMORY_TO_PERIPHERAL;
+  dma_init_struct.memory_base_addr = (uint32_t)usart2_tx_buffer;
+  dma_init_struct.memory_data_width = DMA_MEMORY_DATA_WIDTH_BYTE;
+  dma_init_struct.memory_inc_enable = TRUE;
+  dma_init_struct.peripheral_base_addr = (uint32_t)&USART2->dt;
+  dma_init_struct.peripheral_data_width = DMA_PERIPHERAL_DATA_WIDTH_BYTE;
+  dma_init_struct.peripheral_inc_enable = FALSE;
+  dma_init_struct.priority = DMA_PRIORITY_MEDIUM;
+  dma_init_struct.loop_mode_enable = FALSE;
+  dma_init(DMA1_CHANNEL4, &dma_init_struct);
+
+  // /* dma1 channel5 for usart2 rx configuration */
+  dma_reset(DMA1_CHANNEL5);
+  dma_default_para_init(&dma_init_struct);
+  dma_init_struct.buffer_size = USART2_RX_BUFFER_SIZE;
+  dma_init_struct.direction = DMA_DIR_PERIPHERAL_TO_MEMORY;
+  dma_init_struct.memory_base_addr = (uint32_t)usart2_rx_buffer;
+  dma_init_struct.memory_data_width = DMA_MEMORY_DATA_WIDTH_BYTE;
+  dma_init_struct.memory_inc_enable = TRUE;
+  dma_init_struct.peripheral_base_addr = (uint32_t)&USART2->dt;
+  dma_init_struct.peripheral_data_width = DMA_PERIPHERAL_DATA_WIDTH_BYTE;
+  dma_init_struct.peripheral_inc_enable = FALSE;
+  dma_init_struct.priority = DMA_PRIORITY_MEDIUM;
+  dma_init_struct.loop_mode_enable = FALSE;
+  dma_init(DMA1_CHANNEL5, &dma_init_struct);
+
+  /* configure uart param */
+  usart_init(USART2, baudrate, USART_DATA_8BITS, USART_STOP_1_BIT);
+  usart_transmitter_enable(USART2, TRUE);
+  usart_receiver_enable(USART2, TRUE);
+  usart_interrupt_enable(USART2, USART_IDLE_INT, TRUE);
+  
+
+  usart_dma_transmitter_enable(USART2, TRUE);
+  usart_dma_receiver_enable(USART2, TRUE);
+
+  dma_interrupt_enable(DMA1_CHANNEL5, DMA1_FDT5_FLAG, TRUE);//in dma idle mode, FDT should never happen.
+  nvic_irq_enable(DMA1_Channel5_4_IRQn, 3, 0);
+  nvic_irq_enable(USART2_IRQn, 0, 0);
+  dma_channel_enable(DMA1_CHANNEL5, TRUE); /* usart2 rx begin dma receiving */
+  usart_enable(USART2, TRUE);
+}
+
+void u1rxdma_reset(void)
+{
+  DMA1_CHANNEL3->ctrl_bit.chen = 0; //disable dma channel first
+  DMA1_CHANNEL3->maddr = (uint32_t)usart1_rx_buffer; //reset memory address
+  DMA1_CHANNEL3->dtcnt = USART1_RX_BUFFER_SIZE; //reset dma counter
+  DMA1_CHANNEL3->ctrl_bit.chen = 1; //enable dma channel again
+}
+
+void u2rxdma_reset(void)
+{
+  DMA1_CHANNEL5->ctrl_bit.chen = 0; //disable dma channel first
+  DMA1_CHANNEL5->maddr = (uint32_t)usart2_rx_buffer; //reset memory address
+  DMA1_CHANNEL5->dtcnt = USART2_RX_BUFFER_SIZE; //reset dma counter
+  DMA1_CHANNEL5->ctrl_bit.chen = 1; //enable dma channel again
+}
+
+/**
+  * @brief  this function handles usart2 handler.
+  * @param  none
+  * @retval none
+  */
+void USART1_IRQHandler(void)
+{
+  if ( usart_interrupt_flag_get(USART1, USART_IDLEF_FLAG) )
+  {
+    // usart_data_receive(USART1);
+    usart_flag_clear(USART1, USART_IDLEF_FLAG);
+    DMA1_CHANNEL3->ctrl_bit.chen = 0; //disable dma channel first
+
+    uint16_t rxcnt = USART1_RX_BUFFER_SIZE - DMA1_CHANNEL3->dtcnt;
+    //callback for received data processing
+    rxidle_cb_u1 ?  rxidle_cb_u1(usart1_rx_buffer, rxcnt) : (void) 0;
+    //TODO: enh, use ping pong buffer to avoid data loss
+    u1rxdma_reset();
+  }
+}
+
+void USART2_IRQHandler(void)
+{
+  if ( usart_interrupt_flag_get(USART2, USART_IDLEF_FLAG) )
+  {
+    // usart_data_receive(USART2);
+    usart_flag_clear(USART2, USART_IDLEF_FLAG);
+    DMA1_CHANNEL5->ctrl_bit.chen = 0; //disable dma channel first
+
+    uint16_t rxcnt = USART2_RX_BUFFER_SIZE - DMA1_CHANNEL5->dtcnt;
+    //callback for received data processing
+    rxidle_cb_u2 ?  rxidle_cb_u2(usart2_rx_buffer, rxcnt) : (void) 0;
+    //TODO: enh, use ping pong buffer to avoid data loss
+    u2rxdma_reset();
+  }
+}
+
+// dma full transfer interrupt handler, should never happen in idle dma mode
+void DMA1_Channel3_2_IRQHandler(void)
+{
+  if ( dma_interrupt_flag_get(DMA1_FDT3_FLAG) != RESET )
+  {
+    u1rxdma_reset();
+    dma_flag_clear(DMA1_FDT3_FLAG);
+    printf("FATAL: %s", "dma1 ch3 full transfer aka ovf occur\r\n");
+  }
+}
+
+void DMA1_Channel5_4_IRQHandler(void)
+{
+  if ( dma_interrupt_flag_get(DMA1_FDT5_FLAG) != RESET )
+  {
+    u2rxdma_reset();
+    dma_flag_clear(DMA1_FDT5_FLAG);
+    printf("FATAL: %s", "dma1 ch5 full transfer aka ovf occur\r\n");
+  }
+}
+
+
+/**
+  * @brief  main function.
+  * @param  none
+  * @retval none
+  */
+int main(void)
+{
+  system_clock_config();
+  nvic_priority_group_config(NVIC_PRIORITY_GROUP_4);
+  // dma_configuration();
+  uart1_init(115200);
+  uart2_init(115200);
+  delay_init();
+  LOGI("u1 idle dma example fuck at32\r\n");
+
+  /* transmit a data */
+  while(usart_flag_get(USART1, USART_TDBE_FLAG) == RESET);
+  usart_data_transmit(USART1, 0x55);
+
+  /* receive a data from the tx pin */
+  // while(usart_flag_get(USART1, USART_RDBF_FLAG) == RESET);
+  // usart_data_receive(USART1);
+
+  while(1)
+  {
+    // while(usart_flag_get(USART1, USART_TDBE_FLAG) == RESET);
+    // usart_data_transmit(USART1, 0x55);
+    delay_ms(1000);
+
+    // while(usart_flag_get(USART2, USART_TDBE_FLAG) == RESET);
+    // usart_data_transmit(USART2, 'a');
+  }
+}
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
